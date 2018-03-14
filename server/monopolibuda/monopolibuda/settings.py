b75ad3094aa1a0457e8c2ec1fd5f71e6e40f1407
Tomasz Maka <tomasz.maka@themasters.io>
2018-03-14 11:23:21 +0100

    Add bootstrap

--- server/monopolibuda/monopolibuda/settings.py
+++ server/monopolibuda/monopolibuda/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'monopolibuda',
     'django.contrib.auth',
+    'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
