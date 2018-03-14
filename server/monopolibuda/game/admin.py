a1858e21632bff90ef4fa55c2e9630abf91f2474
jakusz1 <13085272+jakusz1@users.noreply.github.com>
2018-03-14 19:49:55 +0100

    add migrations

--- server/monopolibuda/game/admin.py
+++ server/monopolibuda/game/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Game, Player, Property, Card, Chance, Charge
+
+admin.site.register(Game)
+admin.site.register(Player)
+admin.site.register(Property)
+admin.site.register(Card)
+admin.site.register(Chance)
+admin.site.register(Charge)
--- server/monopolibuda/monopolibuda/settings.py
+++ server/monopolibuda/monopolibuda/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'game',
     'monopolibuda',
     'django.contrib.auth',
     'django.contrib.admin',
