5f3b738e97fa4756b7d0cf6b2b751854787fd677
jakusz1 <13085272+jakusz1@users.noreply.github.com>
2018-03-25 18:56:47 +0200

    add buying offer modal

--- server/monopolibuda/static/js/websocket-client.js
+++ server/monopolibuda/static/js/websocket-client.js
@@ -19,8 +19,9 @@ $(function () {
         //});
 
         //mockup data:
-        $('#properties').append(prepareCard(JSON.parse('{"card_id":2,"buildings":0,"deposited":true,"name":"Budynek B-4","cost":2999,"apartment_cost":500,"hotel_cost":1000,"deposit_value":1000,"group":1,"a0":100,"a1":300,"a2":1000,"a3":3000,"a4":6000,"a5":9000}')));
-        $('#properties').append(prepareCard(JSON.parse('{"card_id":4,"buildings":1,"deposited":false,"name":"Budynek A-1","cost":2999,"apartment_cost":500,"hotel_cost":1000,"deposit_value":1000,"group":3,"a0":100,"a1":300,"a2":1000,"a3":3000,"a4":6000,"a5":9000}')));
+        $('#properties').append(getPreparedCard({"card_id":2,"buildings":0,"deposited":true,"name":"Budynek B-4","cost":2999,"apartment_cost":500,"hotel_cost":1000,"deposit_value":1000,"group":1,"a0":100,"a1":300,"a2":1000,"a3":3000,"a4":6000,"a5":9000}));
+        $('#properties').append(getPreparedCard({"card_id":4,"buildings":1,"deposited":false,"name":"Budynek A-1","cost":2999,"apartment_cost":500,"hotel_cost":1000,"deposit_value":1000,"group":3,"a0":100,"a1":300,"a2":1000,"a3":3000,"a4":6000,"a5":9000}));
+        showPreparedPropertyBuyModal({"card_id":4,"buildings":1,"deposited":false,"name":"Budynek A-1","cost":2999,"apartment_cost":500,"hotel_cost":1000,"deposit_value":1000,"group":3,"a0":100,"a1":300,"a2":1000,"a3":3000,"a4":6000,"a5":9000});
       }
       else
         console.log("Got websocket message " + data);
@@ -37,7 +38,7 @@ $(function () {
     }
   });
 
-  function prepareCard(property){
+  function getPreparedCard(property){
     var template = $('.template').clone();
     var cardHeader = template.find('.card-header');
     var title = template.find('.title');
@@ -66,4 +67,29 @@ $(function () {
     template.find('.cost').html(property.cost);
     
     return template;
+  }
+
+  function showPreparedPropertyBuyModal(property){
+    var modal = $('#cardModal');
+
+    
+    modal.find('.deposit_value').html(property.deposit_value);
+    modal.find('.apartment_cost').html(property.apartment_cost);
+    modal.find('.hotel_cost').html(property.hotel_cost);
+    modal.find('.a0').html(property.a0);
+    modal.find('.a1').html(property.a1);
+    modal.find('.a2').html(property.a2);
+    modal.find('.a3').html(property.a3);
+    modal.find('.a4').html(property.a4);
+    modal.find('.a5').html(property.a5);
+    modal.find('.cost').html(property.cost);
+    modal.find('.name').remove();
+    modal.find('.table').before("<button class='name btn group text-white color-group-"+property.group+"'  type='button'>"+property.name+"   <span class='badge badge-secondary deposited hidden'>Deposited</span></button>")
+    if (property.deposited){
+      modal.find('.deposited').removeClass('hidden');
+    }
+    else{
+      modal.find('.deposited').addClass('hidden');
+    }
+    modal.modal('show');
   }
