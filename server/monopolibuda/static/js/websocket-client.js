29228ef80dfa3fe3396e81bbaa8c7d2de5f101d6
jakusz1 <13085272+jakusz1@users.noreply.github.com>
2018-03-25 15:27:07 +0200

    add property parser

--- server/monopolibuda/static/js/websocket-client.js
+++ server/monopolibuda/static/js/websocket-client.js
@@ -13,10 +13,14 @@ $(function () {
       if(command == "playerdata"){
         //{balance, properties: {[card_id,buildings,deposited,name,cost,apartment_cost,hotel_cost,deposit_value,group,a0,a1,a2,a3,a4,a5]}}
         $('#balance').html(data.balance);
-        data.properties.forEach(element => {
-          var property = $("<a href='#' class='list-group-item list-group-item-action'>"+ element +"</a>");
-          $('#properties').append(property);
-        });
+        //data.properties.forEach(element => {
+          //var property = $("");
+          //$('#properties').append(property);
+        //});
+
+        //mockup data:
+        $('#properties').append(prepareCard(JSON.parse('{"card_id":2,"buildings":0,"deposited":true,"name":"Budynek B-4","cost":2999,"apartment_cost":500,"hotel_cost":1000,"deposit_value":1000,"group":1,"a0":100,"a1":300,"a2":1000,"a3":3000,"a4":6000,"a5":9000}')));
+        $('#properties').append(prepareCard(JSON.parse('{"card_id":4,"buildings":1,"deposited":false,"name":"Budynek A-1","cost":2999,"apartment_cost":500,"hotel_cost":1000,"deposit_value":1000,"group":3,"a0":100,"a1":300,"a2":1000,"a3":3000,"a4":6000,"a5":9000}')));
       }
       else
         console.log("Got websocket message " + data);
@@ -32,3 +36,34 @@ $(function () {
       console.log("Disconnected from socket");
     }
   });
+
+  function prepareCard(property){
+    var template = $('.template').clone();
+    var cardHeader = template.find('.card-header');
+    var title = template.find('.title');
+    var cardBody = template.find('.collapse');
+
+    template.removeClass('template');
+    cardHeader.addClass('color-group-'+property.group);
+    title.html(property.name);
+    title.attr('data-target', '.card-'+property.card_id+'.collapse');
+    template.attr('id', 'card-'+property.card_id);
+    cardBody.addClass('card-'+property.card_id);
+
+    if (property.deposited){
+      template.find('.deposited').removeClass('hidden');
+    }
+    template.find('.buildings').html(property.buildings);
+    template.find('.deposit_value').html(property.deposit_value);
+    template.find('.apartment_cost').html(property.apartment_cost);
+    template.find('.hotel_cost').html(property.hotel_cost);
+    template.find('.a0').html(property.a0);
+    template.find('.a1').html(property.a1);
+    template.find('.a2').html(property.a2);
+    template.find('.a3').html(property.a3);
+    template.find('.a4').html(property.a4);
+    template.find('.a5').html(property.a5);
+    template.find('.cost').html(property.cost);
+    
+    return template;
+  }
